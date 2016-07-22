8ae90a1a77d6d46322f8fc3304e666ef6a617bbc
Harry Shams <harathon@gmail.com>
2016-07-22 13:53:58 +0000

    final project second version

---
+++ application_controller.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+require_relative 'models/model.rb'
+Bundler.require
+
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    @title = "Homepage"
+    erb :index, :layout => :base
+  end
+
+  post '/page2' do
+    @title = "page2"
+	  @lat_lng = format_request_and_send_api_call(params[:user_input])
+	  @places = format_request_and_send_api_call_to_places(@lat_lng[0],@lat_lng[1])
+	  @articles = generate_wiki_link(@places.first)
+    erb :page2
+  end
+
+end
